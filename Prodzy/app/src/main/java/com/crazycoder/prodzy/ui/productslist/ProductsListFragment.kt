2fa602d64c12f24c8ddc2b791c8a4b9b37c90864
AmOL-PiNGALE <curious.luffy@gmail.com>
2022-10-05 15:44:50 +0530

    feat: Initial setup for the project and bottom navigation

---
+++ Prodzy/app/src/main/java/com/crazycoder/prodzy/ui/productslist/ProductsListFragment.kt
@@ -0,0 +1,42 @@
+package com.crazycoder.prodzy.ui.productslist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.crazycoder.prodzy.databinding.FragmentProductsListBinding
+
+class ProductsListFragment : Fragment() {
+
+    private var _binding: FragmentProductsListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val productsListViewModel =
+            ViewModelProvider(this).get(ProductsListViewModel::class.java)
+
+        _binding = FragmentProductsListBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        productsListViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
